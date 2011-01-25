2a28e43e89582dff56280b3898b4f17569b81297
salatgurke00 <salatgurke00@googlemail.com>
2011-01-25 20:43:58 +0000

    Workin

--- pongar/src/conf.h
+++ pongar/src/conf.h
@@ -3,40 +3,14 @@
 #include <cv.h>
 #include <highgui.h>
 
-////thresholding
-//int thresh = 100;
-//int bw_thresh = 100;
-//
-////camera
-//CvCapture* cap;
-//const int width = 640; 
-//const int height = 480;
-////macbook pro camera: camangle = 60
-//const int camangle = 60;
-//
-//float resultMatrix_005A[16];
-//float resultMatrix_0272[16];
-//
-//CvMemStorage* memStorage;
-//unsigned char bkgnd[width*height*3];
-
-//thresholding
-//int thresh;// = 100;
-//int bw_thresh;// = 100;
-
 //camera
-//CvCapture* cap;
 const int width = 640; 
 const int height = 480;
+
 //macbook pro camera: camangle = 60
 const int camangle = 60;
-//
-//float resultMatrix_005A[16];
-//float resultMatrix_0272[16];
-//
-//CvMemStorage* memStorage;
-//unsigned char bkgnd[width*height*3];
+
 
 //UltraTEMP
-
+//TODO remove
 void idle(void);
--- pongar/src/pongar.cpp
+++ pongar/src/pongar.cpp
@@ -1,85 +1,15 @@
 //schtest
 #include "conf.h"
-
 #include "Game.h"
 
 #include <iostream>
 #include <iomanip>
 
 #include "GL/glut.h"
-//
-//#include <cv.h>
-//#include <highgui.h>
 
 #include "PoseEstimation.h"
 
 using namespace std;
-//
-////thresholding
-//int thresh = 100;
-//int bw_thresh = 100;
-//
-////camera
-//CvCapture* cap;
-//const int width = 640; 
-//const int height = 480;
-////macbook pro camera: camangle = 60
-//const int camangle = 60;
-//
-//float resultMatrix_005A[16];
-//float resultMatrix_0272[16];
-//
-//CvMemStorage* memStorage;
-//unsigned char bkgnd[width*height*3];
-//
-////trackbar
-//void trackbarHandler(int pos) {
-//	thresh = pos;
-//}
-//
-//void bw_trackbarHandler(int pos) {
-//	bw_thresh = pos;
-//}
-
-
-//int subpixSampleSafe ( const IplImage* pSrc, CvPoint2D32f p )
-//{
-//
-//	int x = int( floorf ( p.x ) );
-//	int y = int( floorf ( p.y ) );
-//
-//	if ( x < 0 || x >= pSrc->width  - 1 ||
-//		 y < 0 || y >= pSrc->height - 1 )
-//		return 127;
-//
-//	int dx = int ( 256 * ( p.x - floorf ( p.x ) ) );
-//	int dy = int ( 256 * ( p.y - floorf ( p.y ) ) );
-//
-//	unsigned char* i = ( unsigned char* ) ( ( pSrc->imageData + y * pSrc->widthStep ) + x );
-//	int a = i[ 0 ] + ( ( dx * ( i[ 1 ] - i[ 0 ] ) ) >> 8 );
-//	i += pSrc->widthStep;
-//	int b = i[ 0 ] + ( ( dx * ( i[ 1 ] - i[ 0 ] ) ) >> 8 );
-//	return a + ( ( dy * ( b - a) ) >> 8 );
-//}
-
-//void init()
-//{
-//	/*cvNamedWindow ("Original Image", CV_WINDOW_AUTOSIZE);
-//	cvNamedWindow ("Converted", CV_WINDOW_AUTOSIZE);
-//	cvNamedWindow ("Stripe", CV_WINDOW_AUTOSIZE);
-//	cvNamedWindow ("Marker", 0 );
-//	cvResizeWindow("Marker", 120, 120 );
-//	initVideoStream();
-//
-//	int value = thresh;
-//	int max = 255;
-//	cvCreateTrackbar( "Threshold", "Converted", &value, max, trackbarHandler);
-//	
-//	int bw_value = bw_thresh;
-//	cvCreateTrackbar( "BW Threshold", "Converted", &bw_value, max, bw_trackbarHandler);
-//
-//	memStorage = cvCreateMemStorage();*/
-//}
 
 void idle()
 {
@@ -473,139 +403,10 @@ void idle()
 	glutPostRedisplay();
 }
 
-//void cleanup() 
-//{
-//	cvReleaseMemStorage (&memStorage);
-//
-//	cvReleaseCapture (&cap);
-//	cvDestroyWindow ("Original Image");
-//	cvDestroyWindow ("Converted");
-//	cvDestroyWindow ("Stripe");
-//	cvDestroyWindow ("Marker");
-//}
-
-//void display() 
-//{
-//    // clear buffers
-//    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-//
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-//
-//    // draw background image
-//    glDisable( GL_DEPTH_TEST );
-//
-//    glMatrixMode( GL_PROJECTION );
-//    glPushMatrix();
-//    glLoadIdentity();
-//    gluOrtho2D( 0.0, width, 0.0, height );
-//
-//    glRasterPos2i( 0, height-1 );
-//    glDrawPixels( width, height, GL_BGR_EXT, GL_UNSIGNED_BYTE, bkgnd );
-//
-//    glPopMatrix();
-//
-//    glEnable(GL_DEPTH_TEST);
-//
-//    // move to origin
-//    glMatrixMode( GL_MODELVIEW );
-//    
-//	float resultTransposedMatrix[16];
-//	for (int x=0; x<4; ++x)
-//	{
-//		for (int y=0; y<4; ++y)
-//		{
-//			resultTransposedMatrix[x*4+y] = resultMatrix_005A[y*4+x];
-//		}
-//	}
-//
-//	//glLoadTransposeMatrixf( resultMatrix );
-//	glLoadMatrixf( resultTransposedMatrix );
-//	glScalef(0.20, 0.20, 0.20);
-//
-//	// draw white rectangle
-//	glColor4f( 1.0, 1.0, 1.0, 0.7 );
-//	glRectf(-0.5, -0.8, 0.5, 0.8);
-//
-//    // redraw
-//    glutSwapBuffers();
-//}
-
-//void resize( int w, int h) 
-//{
-//	//width = w;
-//	//height = h;
-//
-//    // set a whole-window viewport
-//    glViewport( 0, 0, width, height );
-//
-//    // create a perspective projection matrix
-//    glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//	// Note: Just setting the Perspective is an easy hack. In fact, the camera should be calibrated.
-//	// With such a calibration we would get the projection matrix. This matrix could then be loaded 
-//	// to GL_PROJECTION.
-//	// If you are using another camera (which you'll do in most cases), you'll have to adjust the FOV
-//	// value. How? Fiddle around: Move Marker to edge of display and check if you have to increase or 
-//	// decrease.
-//    gluPerspective( camangle, ((double)width/(double)height), 0.01, 100 );
-//
-//    // invalidate display
-//    glutPostRedisplay();
-//}
 
 int main(int argc, char* argv[]) 
 {
 	Game::getInstance().init(argc, argv);
-
-    //// initialize the window system
-    //glutInit( &argc, argv );
-    //glutInitWindowSize( width, height );
-    //glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH );
-    //glutCreateWindow("PongAR");
-
-    //// initialize the GL library
-
-    //// pixel storage/packing stuff
-    //glPixelStorei( GL_PACK_ALIGNMENT,   1 );
-    //glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
-    //glPixelZoom( 1.0, -1.0 );
-
-    //// enable and set colors
-    //glEnable( GL_COLOR_MATERIAL );
-    //glClearColor( 0, 0, 0, 1.0 );
-
-    //// enable and set depth parameters
-    //glEnable( GL_DEPTH_TEST );
-    //glClearDepth( 1.0 );
-
-    //// light parameters
-    //GLfloat light_pos[] = { 1.0, 1.0, 1.0, 0.0 };
-    //GLfloat light_amb[] = { 0.2, 0.2, 0.2, 1.0 };
-    //GLfloat light_dif[] = { 0.7, 0.7, 0.7, 1.0 };
-
-    //// enable lighting
-    //glLightfv( GL_LIGHT0, GL_POSITION, light_pos );
-    //glLightfv( GL_LIGHT0, GL_AMBIENT,  light_amb );
-    //glLightfv( GL_LIGHT0, GL_DIFFUSE,  light_dif );
-    ////glEnable( GL_LIGHTING );
-    ////glEnable( GL_LIGHT0 );
-
-    //// make functions known to GLUT
-    //glutDisplayFunc( display );
-    //glutReshapeFunc( resize  );
-    //glutIdleFunc( idle );
-
-
-
-
-
-
-    //// setup OpenCV
-    //init();
-
-    // for tracker debugging...
-    //while (1) idle();
 
     // start the action
 
